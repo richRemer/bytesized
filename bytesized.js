8698d770ba06e298ee4aa542fffadcd814641eec
Richard Remer <rich@zingle.me>
2016-07-19 12:36:47 -0700

    negation support and SI units

--- bytesized.js
+++ bytesized.js
@@ -4,19 +4,47 @@
  * @returns {number}
  */
 function bytesized(value) {
-    var parsed;
+    var parsed, neg, base, unit;
 
     if (typeof value === "number") return value;
     
     if (/^-?[0-9]+$/.test(value)) return parseInt(value);
     if (/^-?[0-9]+\.[0-9]+$/.test(value)) return parseFloat(value);
     
-    parsed = /^(-?)([0-9]+(\.[0-9]+)?) ?([a-z]+)$/.exec(value);
+    parsed = /^(-?)([0-9]+(\.[0-9]+)?) ?([a-z]+)$/i.exec(value);
 
     if (parsed) {
+        neg = parsed[1] === "-" ? -1 : 1;
+        base = (parsed[3] ? parseFloat : parseInt)(parsed[2]);
+        unit = unitsize(parsed[4]);
+
+        if (unit === undefined) {
+            throw new Error("invalid format");
+        }
         
+        return neg * base * unit;
     } else {
         throw new Error("invalid format");
+    }
+}
+
+/**
+ * Convert unit to numeric factor.
+ * @param {string} unit
+ * @returns {number}
+ */
+function unitsize(unit) {
+    switch (unit.toLowerCase()) {
+        case "b": return 1;
+        case "kb": return 1000;
+        case "mb": return 1000000;
+        case "gb": return 1000000000;
+        case "tb": return 1000000000000;
+        case "pb": return 1000000000000000;
+        case "eb": return 1000000000000000000;
+        case "zb": return 1000000000000000000000;
+        case "yb": return 1000000000000000000000000;
+        default: return undefined;
     }
 }
 
