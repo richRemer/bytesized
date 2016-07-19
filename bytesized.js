d47c4d27209c1b2277372c1e3a510689df19d76f
Richard Remer <rich@zingle.me>
2016-07-19 12:22:45 -0700

    throw error on invalid values

--- bytesized.js
+++ bytesized.js
@@ -4,10 +4,20 @@
  * @returns {number}
  */
 function bytesized(value) {
+    var parsed;
+
     if (typeof value === "number") return value;
     
     if (/^-?[0-9]+$/.test(value)) return parseInt(value);
     if (/^-?[0-9]+\.[0-9]+$/.test(value)) return parseFloat(value);
+    
+    parsed = /^(-?)([0-9]+(\.[0-9]+)?) ?([a-z]+)$/.exec(value);
+
+    if (parsed) {
+        
+    } else {
+        throw new Error("invalid format");
+    }
 }
 
 module.exports = bytesized;
